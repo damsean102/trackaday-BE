4aec4b4251e0afd3ed7843ae0e41d26b70a509dc
Sam Dean <sam.dean@substrakt.com>
2020-10-06 22:31:25 +0100

    UPDATE: .env variables

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+import os
+import spotipy
+
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy
 from pprint import pprint
-
-import os
 
 load_dotenv()
 
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
+PLAYLIST_URI = os.getenv("SPOTIFY_PLAYLIST_ID")
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=SECRET_ID))
 
-pl_id = 'spotify:playlist:5RmD55dvRIE5AdhzdYwi4b'
 offset = 0
 
 while True:
-    response = sp.playlist_tracks(pl_id, fields='items(track.name,track.artists.name),total', limit=100, offset=offset)
+    response = sp.playlist_tracks(PLAYLIST_URI, fields='items(track(name, artists.name, external_urls.spotify)),total', limit=100, offset=offset)
     pprint(response['items'])
     offset = offset + len(response['items'])
     print(offset, "/", response['total'])
